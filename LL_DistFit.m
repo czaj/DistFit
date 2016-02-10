b2707ce730a8837447f8492b15f6f579b9607b69
ewazawojska <ewa.zawojska@gmail.com>
2016-02-10 12:47:37 +0100

    .
    .

--- LL_DistFit.m
+++ LL_DistFit.m
@@ -3,140 +3,85 @@ function f = LL_DistFit(bounds, dist, RealMin, b0)
 % save CDF_WTP_tmp
 % return
 
-% czy mo?esz wszystkie poprawi? tak jak rozk?ad lognormalny? 
-% to znaczy niech u?ywaj? jednolicie funkcji cdf i do tego tej opcji RealMin
-
 switch dist
 
 % unbounded
-    case 0 % normal % mu, sigma
-        f = log(normcdf(bounds(:,2),b0(1),b0(2)) - normcdf(bounds(:,1),b0(1),b0(2)));        
-    case 1 % logistic
-        pd = fitdist(midpoint,'Logistic','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu, sigma
-    case 2 % Extreme Value
-        pd = fitdist(midpoint,'ExtremeValue','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu sigma
-    case 3 % Generalized Extreme Value
-        pd = fitdist(midpoint,'GeneralizedExtremeValue','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % k, sigma, mu
-    case 4 % tLocationScale
-        pd = fitdist(midpoint,'tLocationScale','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu, sigma, nu
-    case 5 % uniform
-        b0 = [min(midpoint),max(midpoint)];
-    case 6 % Rician
-        pd = fitdist(midpoint,'Rician','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % s, sigma
-    case 7 % Johnson SU
-        pd = f_johnson_fit(midpoint);
-        b0 =  pd.coef; % gamma delta xi lambda
+    case 0 % Nnormal % mu, sigma
+        p = cdf('Normal',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 1 % Logistic % mu, sigma
+        p = cdf('Logistic',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 2 % Extreme Value % mu sigma
+        p = cdf('Extreme Value',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 3 % Generalized Extreme Value % k, sigma, mu 
+        p = cdf('Generalized Extreme Value',bounds,b0(1),b0(2),b0(3));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 4 % tLocationScale % mu, sigma, nu 
+        p = cdf('tLocationScale',bounds,b0(1),b0(2),b0(3));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 5 % Uniform % min, max
+        p = cdf('Uniform',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 6 % Johnson SU % gamma delta xi lambda
+        p = [f_johnson_cdf(bounds(:,1),b0, 'SU'),f_johnson_cdf(bounds(:,2),b0, 'SU')];        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
     % stable
         
 % bounded (0,Inf)
-    case 10 % exponential
-        pd = fitdist(midpoint,'Exponential','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu
-    case 12 % lognormal
+    case 10 % Exponential % mu
+        p = cdf('Exponential',bounds,b0);
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 11 % Lognormal % mu, sigma
         p = cdf('Lognormal',bounds,b0(1),b0(2));        
         f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
-    case 13 % loglogistic
-        pd = fitdist(midpoint,'Loglogistic','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu, sigma
-    case 14 % Weibull
-        pd = fitdist(midpoint,'Weibull','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % A, b0
-    case 11 % Rayleigh
-        pd = fitdist(midpoint,'Rayleigh','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % b0
-    case 15 % Gamma
-        pd = fitdist(midpoint,'Gamma','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % a, b
-    case 16 % b0irnbaumSaunders
-        pd = fitdist(midpoint,'b0irnbaumSaunders','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % beta, gamma
-    case 17 % Generalized Pareto
-        pd = fitdist(midpoint,'GeneralizedPareto','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % k, sigma, theta
-    case 18 % InverseGaussian
-        pd = fitdist(midpoint,'InverseGaussian','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % k, sigma, theta
-    case 19 % Nakagami
-        pd = fitdist(midpoint,'Nakagami','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % mu, omega
-    case 20 % Rician
-        pd = fitdist(midpoint,'Rician','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % s, sigma
-    case 21 % Johnson Sb0
-        pd = f_johnson_fit(midpoint);
-        b0 = pd.coef;
-        %     case x % b0urr
-        %         pd = fitdist(midpoint,'b0urr','Options',OptimOptFit); % Error - Parto distribution fits better
-        %         b0 = pd.ParameterValues; %
+    case 12 % Loglogistic % mu, sigma
+        p = cdf('LogLogistic',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 13 % Weibull % A, b0
+        p = cdf('Weibull',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 14 % Rayleigh % b0
+        p = cdf('Rayleigh',bounds,b0);        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 15 % Gamma % a, b
+        p = cdf('Gamma',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 16 % BirnbaumSaunders % beta, gamma
+        p = cdf('BirnbaumSaunders',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 17 % Generalized Pareto % k, sigma, theta
+        p = cdf('Generalized Pareto',bounds,b0(1),b0(2),b0(3));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 18 % InverseGaussian % k, sigma
+        p = cdf('InverseGaussian',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 19 % Nakagami % mu, omega
+        p = cdf('Nakagami',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 20 % Rician % s, sigma
+        p = cdf('Rician',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 21 % Johnson SB % gamma delta xi lambda % ERROR - values out of range!
+        p = [f_johnson_cdf(bounds(:,1),b0, 'SB'),f_johnson_cdf(bounds(:,2),b0, 'SB')];        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+        %     case x % Burr % Error - Parto distribution fits better
+        %           p = cdf('Burr',bounds,b0(1),b0(2),b0(3));        
+        %           f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
         %     elseif dist==23 % generalized inverse Gaussian
-        % %         b0 =
+        % %         p =
         %     elseif dist==24 % sinh-arcsinh
-        % %         b0 =
+        % %         p =
         
-        % discrete
-    case 31 % Poisson
-        pd = fitdist(midpoint,'Poisson','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % lambda
-    case 32 % negative binomial
-        pd = fitdist(round(midpoint),'Negativeb0inomial','Options',OptimOptFit);
-        b0 = pd.ParameterValues; % R, P
+ % discrete
+    case 31 % Poisson % lambda
+        p = cdf('Poisson',bounds,b0);        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
+    case 32 % Negative Binomial % R, P
+        p = cdf('Negative Binomial',bounds,b0(1),b0(2));        
+        f = RealMin*log(max(eps,p(:,2) - p(:,1))) + ~RealMin*log(p(:,2) - p(:,1));
 end
 
-    
-%     
-% elseif dist==1
-% elseif dist==2
-%     f = log(cdf('Exponential',bounds(:,2),b0) - cdf('Exponential',bounds(:,1),b0));
-% elseif dist==3
-%     f = log(cdf('Logistic',bounds(:,2),b0(1),b0(2)) - cdf('Logistic',bounds(:,1),b0(1),b0(2)));
-% elseif dist==4
-%     f = log(cdf('LogLogistic',bounds(:,2),b0(1),b0(2)) - cdf('LogLogistic',bounds(:,1),b0(1),b0(2)));
-% elseif dist==5 
-% %     f = log(cdf('Poisson',bounds(:,2),b0) - cdf('Poisson',bounds(:,1),b0));
-%     f = log(max(cdf('Poisson',bounds(:,2),b0) - cdf('Poisson',bounds(:,1),b0),realmin));
-% elseif dist==6
-%     f = log(cdf('Weibull',bounds(:,2),b0(1),b0(2)) - cdf('Weibull',bounds(:,1),b0(1),b0(2)));
-% elseif dist==7
-%     f = log(cdf('Gamma',bounds(:,2),b0(1),b0(2)) - cdf('Gamma',bounds(:,1),b0(1),b0(2)));
-% elseif dist==8
-% %     f = log(cdf('Rayleigh',bounds(:,2),b0) - cdf('Rayleigh',bounds(:,1),b0));
-%     f = log(max(cdf('Rayleigh',bounds(:,2),b0) - cdf('Rayleigh',bounds(:,1),b0),realmin));
-% elseif dist==9
-%     f = log(cdf('b0irnbaumSaunders',bounds(:,2),b0(1),b0(2)) - cdf('b0irnbaumSaunders',bounds(:,1),b0(1),b0(2)));
-% elseif dist==10
-%     f = log(cdf('ExtremeValue',bounds(:,2),b0(1),b0(2)) - cdf('ExtremeValue',bounds(:,1),b0(1),b0(2)));
-% elseif dist==11
-%     f = log(cdf('GeneralizedExtremeValue',bounds(:,2),b0(1),b0(2),b0(3)) - cdf('GeneralizedExtremeValue',bounds(:,1),b0(1),b0(2),b0(3)));
-% elseif dist==12
-%     f = log(cdf('GeneralizedPareto',bounds(:,2),b0(1),b0(2)) - cdf('GeneralizedPareto',bounds(:,1),b0(1),b0(2)));
-% elseif dist==13
-%     f = log(cdf('InverseGaussian',bounds(:,2),b0(1),b0(2)) - cdf('InverseGaussian',bounds(:,1),b0(1),b0(2)));
-% elseif dist==14
-%     f = log(cdf('Nakagami',bounds(:,2),b0(1),b0(2)) - cdf('Nakagami',bounds(:,1),b0(1),b0(2)));
-% elseif dist==15
-%     f = log(cdf('Rician',bounds(:,2),b0(1),b0(2)) - cdf('Rician',bounds(:,1),b0(1),b0(2)));
-% elseif dist==16
-%     f = log(cdf('tLocationScale',bounds(:,2),b0(1),b0(2),b0(3)) - cdf('tLocationScale',bounds(:,1),b0(1),b0(2),b0(3)));
-% elseif dist==17 % Johnson SU
-%     f = log(f_johnson_cdf(bounds(:,2),b0, 'SU') - f_johnson_cdf(bounds(:,1),b0, 'SU'));
-% elseif dist==18 % Johnson Sb0 - nie dzia³a, gdy any(u <= 0) || any(u>=1.0)
-%     f = log(f_johnson_cdf(bounds(:,2),b0, 'Sb0') - f_johnson_cdf(bounds(:,1),b0, 'Sb0'));
-% elseif dist==19 % b0urr
-%     f = log(cdf('b0urr',bounds(:,2),b0(1),b0(2),b0(3)) - cdf('b0urr',bounds(:,1),b0(1),b0(2),b0(3)));
-% elseif dist==20 % F
-%     f = log(cdf('F',bounds(:,2),b0(1),b0(2)) - cdf('F',bounds(:,1),b0(1),b0(2)));
-% elseif dist==21 % negative binomial
-%     f = log(cdf('Negative b0inomial',bounds(:,2),b0(1),b0(2)) - cdf('Negative b0inomial',bounds(:,1),b0(1),b0(2)));
-% elseif dist==22 % uniform
-%     f = log(cdf('Uniform',bounds(:,2),b0(1),b0(2)) - cdf('Uniform',bounds(:,1),b0(1),b0(2)));
-% elseif dist==23 % generalized inverse Gaussian
-% %     f = 
-% elseif dist==24 % sinh-arcsinh
-% %     f = 
 end
 
--- demo.m
+++ demo.m
@@ -5,37 +5,38 @@ DATA.WTPfpU(DATA.WTPfpU == 401) = Inf;
 DATA.WTPhpU(DATA.WTPhpU == 401) = Inf;
 
 % dist = ...
-%     0; % normal
-%     1; % lognormal
-%     2; % exponential
-%     3; % logistic
-%     4; % loglogistic
-%     5; % Poisson
-%     6; % Weibull
-%     7; % Gamma
-%     8; % Rayleigh
-%     9; % BirnbaumSaunders
-%     10; % Extreme Value
-%     11; % Generalized Extreme Value
-%     12; % Generalized Pareto
-%     13; % Inverse Gaussian
-%     14; % Nakagami
-%     15; % Rician
-%     16; % tLocationScale
-%     17; % Johnson SU
-%     18; % Johnson SB
-%     19; % Burr
-%     20; % F 
-%     21; % negative binomial
-%     22; % uniform
-%     23; % generalized inverse Gaussian
-%     24; % sinh-arcsinh
+%     0; % Normal
+%     1; % Logistic
+%     2; % Extreme Value
+%     3; % Generalized Extreme Value
+%     4; % tLocationScale
+%     5; % Uniform
+%     6; % Johnson SU
+
+%     10; % Expotential
+%     11; % Lognormal
+%     12; % Loglogistic
+%     13; % Weibull
+%     14; % Rayleigh
+%     15; % Gamma
+%     16; % BirnbaumSaunders
+%     17; % Generalized Pareto
+%     18; % Inverse Gaussian
+%     19; % Nakagami
+%     20; % Rician
+%     21; % Johnson SB
+
+%     31; % Poisson
+%     32; % Negative Binomial
     
 INPUT.bounds = [DATA.WTPfpL DATA.WTPfpU];
 
 
 % WTP = WTPfit(INPUT);
 
+% Te nazwy poni¿ej do zmiany, bo zmieni³a siê kolejnoœæ. Ale nie jestem
+% pewna, jak poprawiæ, bo numery dist nie s¹ kolejne, a to chyba wp³ywa na
+% wykorzystanie wektora nazw do pêtli poni¿ej.
 names = {'normal' 'lognormal' 'exponential' 'logistic' 'loglogistic' 'Poisson' 'Weibull' 'Gamma' 'Rayleigh' 'BirnbaumSaunders' 'ExtremeValue' 'GeneralizedExtremeValue' 'GeneralizedPareto' 'InverseGaussian' 'Nakagami' 'Rician' 'tLocationScale' 'JohnsonSU' 'JohnsonSB' 'Burr' 'F' 'NegativeBinomial' 'uniform' 'GeneralizedInverseGaussian' 'sinh-arcsinh'};
 
 for i = 1:size(names,2);
